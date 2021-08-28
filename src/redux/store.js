b4b2b87f55efe6e27a14d0323b20e27ad93de784
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-08-28 08:27:36 +0300

    install redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contactsReducer from 'redux/slices/contactsSlice';
 import filterReducer from 'redux/slices/filterSlice';
 import { configureStore } from '@reduxjs/toolkit';
@@ -8,8 +10,18 @@ const rootReducer = combineReducers({
   filter: filterReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
-export default store;
+const persistor = persistStore(store);
+console.log(persistor);
+
+export const persistedStore = { store, persistor };
