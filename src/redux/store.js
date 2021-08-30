1da808524899e9b884808e451409658f3da09e26
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-08-30 21:24:38 +0300

    +add: register form, authApi requests

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,18 @@
 import { combineReducers } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import contactsReducer from 'redux/slices/contactsSlice';
 import filterReducer from 'redux/slices/filterSlice';
-import { configureStore } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -13,15 +22,19 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
+export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-const persistor = persistStore(store);
-console.log(persistor);
-
-export const persistedStore = { store, persistor };
+export const persistor = persistStore(store);
